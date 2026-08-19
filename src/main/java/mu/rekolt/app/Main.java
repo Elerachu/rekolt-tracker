7fa303f5d30a3e8e3356c61462cc0e0c62af47d4
EleraChu <elerajchu@gmail.com>
2026-08-19 14:50:08 +0400

    Convert deliveries from String[][] to ArrayList<Delivery>; update startup loop to use Delivery getters

--- src/main/java/mu/rekolt/app/Main.java
+++ src/main/java/mu/rekolt/app/Main.java
@@ -6,35 +6,35 @@ import mu.rekolt.service.PaymentService;
 import mu.rekolt.service.PriceService;
 import mu.rekolt.util.InputValidator;
 
+import java.util.ArrayList;
+import java.util.List;
+import mu.rekolt.model.Delivery;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[][] deliveries = {
-                {"M-0032", "Mia Gray", "BNS", "780", "95", "17"},
-                {"M-0082", "Tosin", "TEA", "1200", "89", "4"},
-                {"M-0048", "Ubong", "BNS", "780", "72", "6"},
-                {"M-0025", "Bantu", "MZE", "375", "22", "20"},
-                {"M-0016", "Dapson", "POT", "150", "12", "11"},
-                {"M-0054", "Wale", "BNS", "1500", "36", "9"},
-                {"M-0103", "Becca", "TEA", "238", "42", "1"},
-                {"M-0008", "Chiamaka", "POT", "4589", "61", "17"},
-                {"M-0061", "Minata", "MZE", "643", "95", "4"},
-                {"M-0041", "Lu Xie", "TEA", "38", "95", "20"},
-                {"M-0023", "Kang", "BNS", "12", "95", "15"},
-                {"M-0108", "Dongju", "MZE", "154", "95", "6"}
-        };
+        List<Delivery> deliveries = new ArrayList<>();
+        deliveries.add(new Delivery("M-0032", "Mia Gray", "BNS", 780, 95, 17));
+        deliveries.add(new Delivery("M-0082", "Tosin", "TEA", 1200, 89, 4));
+        deliveries.add(new Delivery("M-0048", "Ubong", "BNS", 780, 72, 6));
+        deliveries.add(new Delivery("M-0025", "Bantu", "MZE", 375, 22, 20));
+        deliveries.add(new Delivery("M-0016", "Dapson", "POT", 150, 12, 11));
+        deliveries.add(new Delivery("M-0054", "Wale", "BNS", 1500, 36, 9));
+        deliveries.add(new Delivery("M-0103", "Becca", "TEA", 238, 42, 1));
+        deliveries.add(new Delivery("M-0008", "Chiamaka", "POT", 4589, 61, 17));
+        deliveries.add(new Delivery("M-0061", "Minata", "MZE", 643, 95, 4));
+        deliveries.add(new Delivery("M-0041", "Lu Xie", "TEA", 38, 95, 20));
+        deliveries.add(new Delivery("M-0023", "Kang", "BNS", 12, 95, 15));
+        deliveries.add(new Delivery("M-0108", "Dongju", "MZE", 154, 95, 6));
 
         double[][] weeklyGrid = new double[21][4];
         double seasonTotal = 0;
 
         // Process the season's hardcoded deliveries once at startup
-        for (String[] delivery : deliveries) {
+        for (Delivery delivery : deliveries) {
             seasonTotal += PaymentService.processDelivery(delivery);
-            int deliveryWeek = Integer.parseInt(delivery[5]);
-            String deliveryProduceCode = delivery[2];
-            double deliveryMass = Double.parseDouble(delivery[3]);
-            weeklyGrid[deliveryWeek][PriceService.columnFor(deliveryProduceCode)] += deliveryMass;
+            weeklyGrid[delivery.getWeek()][PriceService.columnFor(delivery.getProduceCode())] += delivery.getMass();
         }
 
         System.out.println("REKOLT PRODUCE TRACKER - season 2026");
